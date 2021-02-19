44dab87004533f58f8632298eebdd04568e9b6e5
Ruben539 <78937675+Ruben539@users.noreply.github.com>
2021-02-19 14:15:32 -0300

    Add files via upload

---
+++ Joyeria/js/codigo.js
@@ -0,0 +1,12 @@
+//Comienzo del codigo JavaScrip
+
+const altura = document.body.scrollHeight - window.innerHeight;
+const fondo = document.getElementById('fondo');
+
+window.onscroll = () => {
+	const anchoFondo = (window.pageYOffset / altura) * 700;
+if (anchoFondo <= 100) {
+	fondo.style.width = anchoFondo + '%';
+ }
+	
+} 
