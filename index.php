9b69b77579b4b255136801edd6d022e8f676d4da
Ruben539 <78937675+Ruben539@users.noreply.github.com>
2021-04-06 21:07:02 -0400

    prueba

---
+++ index.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <link rel="shortcut icon" href="#" />
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>División Industrial</title>
+    
+     <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="estilos.css">
+    
+    <link rel="stylesheet" href="plugins/sweet_alert2/sweetalert2.min.css">
+</head>
+<body>
+    <div id="login">
+        <h3 class="text-center  display-4">División Industrial</h3>
+        <img src="img/alex.png">
+        <div class="container">                        
+            <div id="login-row" class="row justify-content-center align-items-center">
+                <div id="login-column" class="col-md-6">
+                    <div id="login-box" class="col-md-12  bg-light text-dark">
+                        <form id="formLogin" class="form" action="" method="post">
+                            <h3 class="text-center ">Iniciar Sesión</h3>
+                            <div class="form-group">
+                                <label for="usuario" class="text-dark"><b>Usuario :</b></label><br>
+                                <input type="text" name="usuario" id="usuario" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="password" class="text-dark"><b>Password :</b></label><br>
+                                <input type="password" name="password" id="password" class="form-control">
+                            </div>
+                            <div class="form-group text-center">                                
+                                <input type="submit" name="submit" class="btnok  btn-lg btn-block" value="Ingresar">
+                            </div>                            
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="jquery/jquery-3.3.1.min.js"></script>
+    <script src="popper/popper.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+    
+    <script src="plugins/sweet_alert2/sweetalert2.all.min.js"></script>
+    <script src="codigo.js"></script>
+</body>
+</html>
