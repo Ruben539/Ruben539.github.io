d2acac63f58707b8123658cc25580aa4273f6b56
Ruben539 <78937675+Ruben539@users.noreply.github.com>
2022-02-09 19:38:19 -0300

    prueba 1

--- Asses/js/function_login.js
+++
@@ -1,5 +0,0 @@
-// Login Page Flipbox control
-$('.login-content [data-toggle="flip"]').click(function() {
-  $('.login-box').toggleClass('flipped');
-  return false;
-});
--- Librerias/host.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$usuarioBD="root";
-$passBD="";
-$ipBD="127.0.0.1";
-$nombreBD= "paula";
